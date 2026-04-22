1000282b206a968ad6317ebc47ecfe8574d324a1
singhsujay866-web <sujaysingh145@gmail.com>
2026-04-22 10:44:07 +0530

    min-max feature

--- src/components/Windows/MacWindows.jsx
+++ src/components/Windows/MacWindows.jsx
@@ -1,26 +1,35 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Rnd } from 'react-rnd'
 import "./windows.scss"
-const MacWindows = ({ children, width = "30vw", height = "60vh" , windowsName,setwindowsState}) => {
+
+const MacWindows = ({ children, width = "30vw", height = "60vh", windowsName, setwindowsState }) => {
+    const rndRef = useRef(null);
+
+    const handleFullscreen = () => {
+        const el = rndRef.current?.resizableElement?.current;
+        if (!el) return;
+        if (!document.fullscreenElement) {
+            el.requestFullscreen();
+        } else {
+            document.exitFullscreen();
+        }
+    };
+
     return (
         <Rnd
-            default={{
-                x: 100,
-                y: 100,
-                width: width,
-                height: height,
-            }}
+            ref={rndRef}
+            default={{ x: 100, y: 100, width, height }}
             minWidth="280px"
             minHeight="350px"
         >
             <div className="windows">
                 <div className="nav">
                     <div className="dots">
-                        <div 
-                        onClick={()=>{setwindowsState(state=>({...state,[windowsName]:false}))}}
-                        className="dot red"></div>
-                        <div className="dot yellow"></div>
-                        <div className="dot green"></div>
+                        <div onClick={() => setwindowsState(state => ({...state, [windowsName]: false}))}
+                            className="dot red" />
+                        <div className="dot yellow" />
+                        <div onClick={handleFullscreen}
+                            className="dot green" />
                     </div>
                     <div className="title">
                         <p>sujaysingh - zsh</p>
